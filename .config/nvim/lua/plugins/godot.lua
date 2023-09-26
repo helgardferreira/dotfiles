46ed1eab7bc1c9e29a24932d52b6c495b891ce72
Helgard Ferreira <hrfer3@gmail.com>
2023-09-26 19:24:41 +0200

    feat: add gdscript support

---
+++ .config/nvim/lua/plugins/godot.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "gdscript",
+          "godot_resource",
+        })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- gdscript will be automatically installed with mason and loaded with lspconfig
+        gdscript = {
+          flags = {
+            debounce_text_changes = 150,
+          }
+        },
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.register({
+        null_ls.builtins.diagnostics.gdlint,
+        null_ls.builtins.formatting.gdformat,
+      })
+    end,
+  },
+  {
+    "habamax/vim-godot",
+  },
+}
