8df1c86f4b2711a4a222deb175cd917dc73b85d5
Helgard Ferreira <hrfer3@gmail.com>
2023-09-29 17:28:13 +0200

    chore: update plugins

--- .config/nvim/lua/plugins/godot.lua
+++ .config/nvim/lua/plugins/godot.lua
@@ -24,7 +24,7 @@ return {
     },
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     config = function()
       local null_ls = require("null-ls")
       null_ls.register({
