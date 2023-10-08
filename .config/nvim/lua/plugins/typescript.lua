4ff0e5ef07a4fe782a7836dc2790d2700beb1598
Helgard Ferreira <hrfer3@gmail.com>
2023-10-08 16:16:06 +0200

    feat: add json, tailwindcss, and typescript / ecmascript support

---
+++ .config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      ---@type lspconfig.options
+      servers = {
+        denols = { root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc") },
+        ---@type lspconfig.options.tsserver
+        tsserver = {
+          root_dir = require("lspconfig").util.root_pattern("package.json"),
+          single_file_support = false,
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
